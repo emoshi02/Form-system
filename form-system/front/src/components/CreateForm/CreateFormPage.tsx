a02ce7f2359d287386002aa66511c37239ff7797
emoshi02 <emilijarack49@gmail.com>
2024-06-21 11:47:48 +0100

    Reuse SubHeader component

--- form-system/front/src/components/CreateForm/CreateFormPage.tsx
+++ form-system/front/src/components/CreateForm/CreateFormPage.tsx
@@ -1,12 +1,12 @@
-import { Header } from "../Header/Header";
-import { SubHeader } from "../SubHeader/SubHeader";
-import { CreateFormBody } from "./CreateFormBody/CreateFormBody";
+import { Header } from '../Header/Header';
+import { SubHeader } from '../SubHeader/SubHeader';
+import { CreateFormBody } from './CreateFormBody/CreateFormBody';
 
 export const CreateFormPage = () => {
   return (
     <>
       <Header />
-      <SubHeader />
+      <SubHeader item1="Questions" item2="Answers" />
       <CreateFormBody />
     </>
   );
