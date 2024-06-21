f655367b03a79c36a414eeff2d33c88b7eb83d58
emoshi02 <emilijarack49@gmail.com>
2024-06-21 11:46:53 +0100

    create browser router

--- form-system/front/src/App.tsx
+++ form-system/front/src/App.tsx
@@ -1,14 +1,25 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { MainPage } from './components/MainPage/MainPage';
 import { LoginPage } from './components/LoginPage/Login';
-// import { CreateFormPage } from "./components/CreateForm/CreateFormPage";
-// import { MainPage } from "./components/MainPage/MainPage";
+import { CreateFormPage } from './components/CreateForm/CreateFormPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginPage />,
+  },
+  {
+    path: '/main',
+    element: <MainPage />,
+  },
+  {
+    path: '/createForm',
+    element: <CreateFormPage />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <LoginPage />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
