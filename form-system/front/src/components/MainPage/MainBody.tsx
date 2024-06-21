418ddbc8a7d6dfe1afbd1f0221531f8576210fba
emoshi02 <emilijarack49@gmail.com>
2024-06-21 11:51:05 +0100

    change style of the Main Page

--- form-system/front/src/components/MainPage/MainBody.tsx
+++ form-system/front/src/components/MainPage/MainBody.tsx
@@ -1,20 +1,8 @@
-import "./MainPage.scss";
+import './MainPage.scss';
 
 export const MainBody = () => {
   return (
     <main className="main-body">
-      <span className="sub-header">
-        <section className="sub-header-item">
-          <span className="main-page-sub-title">
-            <p>Naujausios formos</p>
-          </span>
-        </section>
-        <section className="sub-header-item">
-          <span className="main-page-sub-title">
-            <p>Man priskirtos formos</p>
-          </span>
-        </section>
-      </span>
       <section className="create-form">
         <button className="create-form-btn">
           <img
