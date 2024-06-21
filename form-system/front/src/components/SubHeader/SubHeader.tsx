85e6f253d395ee9752e65f5d5f0ae81394fbbee5
emoshi02 <emilijarack49@gmail.com>
2024-06-21 11:50:51 +0100

    Add props to SubHeader

--- form-system/front/src/components/SubHeader/SubHeader.tsx
+++ form-system/front/src/components/SubHeader/SubHeader.tsx
@@ -1,13 +1,18 @@
-import "./SubHeader.scss";
+import './SubHeader.scss';
 
-export const SubHeader = () => {
+interface SubHeaderProps {
+  item1: string;
+  item2: string;
+}
+
+export const SubHeader = ({ item1, item2 }: SubHeaderProps) => {
   return (
     <nav className="secondary-header">
       <a className="header-nav-item active" id="header-nav-item-first">
-        <p className="header-nav-item-p">Questions</p>
+        <p className="header-nav-item-p">{item1}</p>
       </a>
       <a className="header-nav-item" id="header-nav-item-second">
-        <p className="header-nav-item-p">Answers</p>
+        <p className="header-nav-item-p">{item2}</p>
       </a>
     </nav>
   );
