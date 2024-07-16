07ff4d3f4cf2073b00e6f9ccb6129f252b26313c
emoshi02 <emilijarack49@gmail.com>
2024-07-16 19:21:26 +0100

    Add fake form item data and basic functionality

--- form-system/front/src/components/MainPage/FormItem/FormItem.tsx
+++ form-system/front/src/components/MainPage/FormItem/FormItem.tsx
@@ -1,19 +1,20 @@
-import "./FormItem.scss";
+import './FormItem.scss';
 
 export const FormItem = ({
   form_id,
   form_title,
+  onDeleteBtn,
 }: {
   form_id: number;
   form_title: string;
+  onDeleteBtn: () => void;
 }) => {
   return (
     <div className="form-item-wrapper">
       <div className="form-item-image" id={`form-image ${form_id}`}>
         <img
-          src="../../../assets/images/form-image.png"
-          alt="Form Image"
-          className="form-image--small"
+          src="https://ssl.gstatic.com/docs/spreadsheets/forms/forms_icon_2023q4.ico"
+          alt={`form ${form_id}`}
         />
       </div>
       <div className="form-item-title">
@@ -21,6 +22,7 @@ export const FormItem = ({
         <span
           className="material-symbols-outlined form-item-delete-btn"
           id={`button${form_id}`}
+          onClick={onDeleteBtn}
         >
           delete
         </span>
--- form-system/front/src/components/MainPage/MainBody.tsx
+++ form-system/front/src/components/MainPage/MainBody.tsx
@@ -1,7 +1,26 @@
-import './MainPage.scss';
+import { useEffect, useState } from 'react';
+import { FormItem } from './FormItem/FormItem';
 import { useNavigate } from 'react-router-dom';
 import './MainPage.scss';
 
+const RECENT_FORMS = [
+  { id: 0, title: 'AAA' },
+  { id: 1, title: 'BBB' },
+  { id: 2, title: 'CCC' },
+  { id: 3, title: 'DDD' },
+  { id: 4, title: 'EEE' },
+  { id: 5, title: 'FFF' },
+  { id: 6, title: 'GGG' },
+  { id: 7, title: 'HHH' },
+  { id: 8, title: 'III' },
+];
+
+const RECEIVED_FORMS = [
+  { id: 0, title: 'AAA' },
+  { id: 1, title: 'BBB' },
+  { id: 2, title: 'CCC' },
+  { id: 8, title: 'III' },
+];
 
 export const MainBody = ({
   activeSectionIndex,
@@ -22,8 +41,23 @@ export const MainBody = ({
     navigate('/createForm');
   };
 
+  const handleDeleteBtnClick = (index: number) => {
+    const updatedForms = forms.filter((_, i) => i !== index);
+    setForms(updatedForms);
+  };
+
   return (
     <main className="main-body">
+      <section className="forms">
+        {forms.map((form, index) => (
+          <FormItem
+            form_id={form.id}
+            form_title={form.title}
+            onDeleteBtn={() => handleDeleteBtnClick(index)}
+            key={form.id}
+          />
+        ))}
+      </section>
       <section className="create-form">
         <button className="create-form-btn" onClick={onBtnClick}>
           <img
