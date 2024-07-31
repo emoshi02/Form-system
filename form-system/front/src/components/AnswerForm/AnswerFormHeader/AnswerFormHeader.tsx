bf8aad1bafacc5df8411d16c79cd6097221f9a30
emoshi02 <emilijarack49@gmail.com>
2024-07-31 15:38:54 +0300

    Change required text

--- form-system/front/src/components/AnswerForm/AnswerFormHeader/AnswerFormHeader.tsx
+++ form-system/front/src/components/AnswerForm/AnswerFormHeader/AnswerFormHeader.tsx
@@ -14,7 +14,7 @@ export const AnswerFormHeader = ({
       <h1 className="answer-form-title">{title}</h1>
       <p className="answer-form-desc">{desc}</p>
       <p className="answer-form-user">{user}</p>
-      <p className="required">* Indicates the Required Question</p>
+      <p className="required">* Indicates the Required Question.</p>
     </section>
   );
 };
