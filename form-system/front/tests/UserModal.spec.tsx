79ef838c704a48795f4828af72681df2012f6724
emoshi02 <emilijarack49@gmail.com>
2024-08-06 14:21:40 +0300

    Add UserModal test

---
+++ form-system/front/tests/UserModal.spec.tsx
@@ -0,0 +1,35 @@
+import { describe, expect, test, jest } from '@jest/globals';
+import { render, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { UserModal } from '../src/components/UserModal/UserModal';
+import * as router from 'react-router';
+
+describe('User Modal', () => {
+  test('Navigate to Main page', async () => {
+    const navigateMock = jest.fn();
+
+    jest.spyOn(router, 'useNavigate').mockImplementation(() => navigateMock);
+
+    const component = render(
+      <BrowserRouter>
+        <UserModal setOpenModal={() => true} />
+      </BrowserRouter>,
+    );
+
+    fireEvent.click(component.getByText('Logout'));
+    expect(navigateMock).toHaveBeenCalledTimes(1);
+    expect(navigateMock).toHaveBeenCalledWith('/');
+  });
+
+  test('Close User modal', async () => {
+    const onModalCloseMock = jest.fn();
+    const component = render(
+      <BrowserRouter>
+        <UserModal setOpenModal={onModalCloseMock} />
+      </BrowserRouter>,
+    );
+    fireEvent.click(component.getByText('Cancel'));
+    expect(onModalCloseMock).toHaveBeenCalledTimes(1);
+    expect(onModalCloseMock).toHaveBeenCalledWith(false);
+  });
+});
