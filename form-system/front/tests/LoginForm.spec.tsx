ab9a33c71f377d11750ea9156714441c52ab4029
emoshi02 <emilijarack49@gmail.com>
2024-08-10 10:36:57 +0300

    Add login form test

---
+++ form-system/front/tests/LoginForm.spec.tsx
@@ -0,0 +1,23 @@
+import { describe, expect, jest, test } from '@jest/globals';
+import { render, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import * as router from 'react-router';
+import { LoginForm } from '../src/components/LoginPage/LoginForm/LoginForm';
+
+describe('Login form', () => {
+  test('Navigate to Main page', async () => {
+    const navigateMock = jest.fn();
+
+    jest.spyOn(router, 'useNavigate').mockImplementation(() => navigateMock);
+
+    const component = render(
+      <BrowserRouter>
+        <LoginForm />
+      </BrowserRouter>,
+    );
+
+    fireEvent.submit(component.getByText('Login'));
+    expect(navigateMock).toHaveBeenCalledTimes(1);
+    expect(navigateMock).toHaveBeenCalledWith('/main');
+  });
+});
