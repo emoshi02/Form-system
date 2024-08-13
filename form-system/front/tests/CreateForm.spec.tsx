7b01de905ef866354a40c18d112fb59424276096
emoshi02 <emilijarack49@gmail.com>
2024-08-13 09:57:18 +0300

    Add Create form test

---
+++ form-system/front/tests/CreateForm.spec.tsx
@@ -0,0 +1,72 @@
+import { describe, expect, test } from '@jest/globals';
+import { render, fireEvent } from '@testing-library/react';
+import { CreateForm } from '../src/components/CreateForm/CreateFormBody/CreateForm';
+import { BrowserRouter } from 'react-router-dom';
+import { FORM_DATA } from './testData';
+
+describe('Create form', () => {
+  test('Form empty state renders when Form data is null', async () => {
+    const component = render(
+      <BrowserRouter>
+        <CreateForm formData={null} />
+      </BrowserRouter>,
+    );
+
+    expect(
+      component.container
+        .getElementsByClassName('form-title')[0]
+        .getAttribute('value'),
+    ).toBe('Untitled Form');
+  });
+
+  test('Null Form on submit prevents default', async () => {
+    const component = render(
+      <BrowserRouter>
+        <CreateForm formData={null} />
+      </BrowserRouter>,
+    );
+
+    const preventDefault = jest.fn();
+
+    fireEvent.submit(component.container.getElementsByTagName('form')[0], {
+      preventDefault,
+    });
+
+    expect(preventDefault).toHaveBeenCalledTimes(0);
+  });
+
+  test('Form on submit prevents default', async () => {
+    const component = render(
+      <BrowserRouter>
+        <CreateForm formData={FORM_DATA} />
+      </BrowserRouter>,
+    );
+
+    const preventDefault = jest.fn();
+
+    const element = component.container.getElementsByTagName('form')[0];
+    element.addEventListener('submit', preventDefault);
+    fireEvent.submit(element);
+
+    expect(preventDefault).toHaveBeenCalledTimes(1);
+  });
+
+  test('Form state updates correctly', async () => {
+    const component = render(
+      <BrowserRouter>
+        <CreateForm formData={FORM_DATA} />
+      </BrowserRouter>,
+    );
+
+    fireEvent.change(
+      component.container.getElementsByClassName('form-title')[0],
+      { target: { value: 'Test Title' } },
+    );
+
+    expect(
+      component.container
+        .getElementsByClassName('form-title')[0]
+        .getAttribute('value'),
+    ).toBe('Test Title');
+  });
+});
